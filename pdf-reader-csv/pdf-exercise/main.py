ff5431023a0dd2e9e000bc5989354b8f8ec07da1
Renato <renato92.hora@gmail.com>
2024-07-03 15:18:23 -0300

    Finalize project

--- pdf-reader-csv/pdf-exercise/main.py
+++ pdf-reader-csv/pdf-exercise/main.py
@@ -6,10 +6,13 @@ filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
-    with open(f"{filepath}") as my_file:
-        pdf.add_page()
-        filename = Path(filepath).stem
-        pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, txt=f"{filename.title()}")
+    with open(f"{filepath}") as file:
+        content = file.read()
+    pdf.add_page()
+    filename = Path(filepath).stem
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"{filename.title()}", ln=1)
+    pdf.set_font(family="Times", size=10)
+    pdf.multi_cell(w=0, h=6, txt=f"{content}")
 
 pdf.output(f"PDFs/Animals.pdf")
